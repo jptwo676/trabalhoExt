d0e0f30ca9469b78ac18a5df2a9e907218dbd2c5
jptwo676 <joaobtwo@gmail.com>
2024-10-18 17:20:44 -0300

    18/10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("teste");
+
+//seleciona o elemento
+var conteudoPrincipal = document.querySelector("#conteudo");
+
+conteudoPrincipal.textContent = "Teste1";
+console.log(conteudoPrincipal);
