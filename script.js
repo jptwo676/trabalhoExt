f054f65329919bf24bd5f8a3f1f92284363f6a9d
jptwo676 <joaobtwo@gmail.com>
2024-10-28 20:28:07 -0300

    add script after html elements and changes on grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("teste");
 
 //seleciona o elemento
-var conteudoPrincipal = document.querySelector("#conteudo");
+var conteudoPrincipal = document.querySelector("#texto");
+console.log(conteudoPrincipal);
 
 conteudoPrincipal.textContent = "Teste1";
 console.log(conteudoPrincipal);
