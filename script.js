467ad6bb11d72b7a9dc758fc58dbdc3de407bcc8
jptwo676 <joaobtwo@gmail.com>
2024-11-07 14:45:34 -0300

    07/11

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ var conteudoInicio = document.getElementById("infoInicio");
 var conteudoContato = document.getElementById("infoContato")
 var titulo = document.getElementById("titulo");
 
+conteudoInicio.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas."
+
 botaoInicio.addEventListener("click", function(){
     titulo.textContent = "Início"
+    conteudoInicio.textContent = "";
+    conteudoContato.textContent = "";
     conteudoInicio.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas."
-    console.log("botao inicio ativado")
     
 });
 
 botaoInfo.addEventListener("click", function(){
+    conteudoInicio.textContent = "";
     console.log("botao info ativado")
 
 });
 
 botaoContato.addEventListener("click", function(){
-    console.log("botao contato ativado")
+    conteudoInicio.textContent = "";
+    conteudoContato.textContent = "";
     titulo.textContent = "Entre em contato!"
     conteudoContato.textContent = "Instagram: @acamadosmaisamadosofc Telefone: (43) 99922-3245"
 });
