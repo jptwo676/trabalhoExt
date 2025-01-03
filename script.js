54bfc1d76702f8f07ac05c5c0039430fc8996c52
jptwo676 <joaobtwo@gmail.com>
2025-01-03 14:25:49 -0300

    hover for buttons

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var conteudoContato = document.getElementById("infoContato")
 var conteudoInfo = document.getElementById("infoInfo");
 var titulo = document.getElementById("titulo");
 
-conteudoInicio.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas."
+conteudoInicio.textContent = `O projeto acamados + amados é uma iniciativa não
+  governamental que apoia familias e cuidadores de pessoas acamadas.`;
 
 botaoInicio.addEventListener("click", function(){
   console.log("inicio on")
