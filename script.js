13ddea75512fdbd856d65d8e82573da89b0d05f5
jptwo676 <joaobtwo@gmail.com>
2025-01-03 14:15:40 -0300

    comments on css

--- script.js
+++ script.js
@@ -9,26 +9,37 @@ var titulo = document.getElementById("titulo");
 conteudoInicio.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas."
 
 botaoInicio.addEventListener("click", function(){
-    console.log("inicio on")
-    titulo.textContent = "Início"
-    conteudoInicio.textContent = "";
-    conteudoContato.textContent = "";
-    conteudoInicio.textContent = ""
-    
+  console.log("inicio on")
+  titulo.textContent = "Início"
+  conteudoInicio.textContent = "bla bla";
+  conteudoInfo.textContent = "";
+  conteudoContato.textContent = "";
+  
 });
 
 botaoInfo.addEventListener("click", function(){
-    conteudoInfo.textContent = `O projeto acamados + amados é uma iniciativa 
-  não governamental que apoia familias e cuidadores de pessoas acamadas.`;
-    console.log("info on")
+  conteudoInicio.textContent = "";
+  conteudoContato.textContent = "";
+  conteudoInfo.textContent = `O projeto Acamados + Amados é uma iniciativa 
+  não governamental que apoia familias e cuidadores de pessoas acamadas.
+    Através de reuniões quinzenais o projeto vem distribuindo fraldas, alimentos
+  equipamentos méidicos e também servindo como um grupo de apoio para os 
+  acamados e seus cuidadores.
+
+    Idealizado por Dona Inês, o projeto existe a mais de 15 anos e conta com 
+  varios voluntarios e parceiros. Com a ajuda prestada o projeto presta tambem
+  atendimento odontologico e faz visitas regulares as casas cadastradas para
+  se certificar que as orientações estão sendo seguidas e ajudar no que for 
+  necessario.`;
+  console.log("info on")
 
 });
 
 botaoContato.addEventListener("click", function(){
-    console.log("contato on")
-    conteudoInicio.textContent = "";
-    conteudoContato.textContent = "";
-    titulo.textContent = "Entre em contato!"
-    conteudoContato.textContent = "Instagram: @acamadosmaisamadosofc Telefone: (43) 99922-3245"
+  console.log("contato on")
+  conteudoInicio.textContent = "";
+  conteudoInfo.textContent = "";
+  titulo.textContent = "Entre em contato!"
+  conteudoContato.textContent = "Instagram: @acamadosmaisamadosofc Telefone: (43) 99922-3245"
 });
 
