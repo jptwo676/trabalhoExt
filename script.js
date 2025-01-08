c40d3a8dbaa9a5e1a14877f92d433561e06f2d4c
jptwo676 <joaobtwo@gmail.com>
2025-01-08 20:27:57 -0300

    title font

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var botaoInicio = document.getElementById("inicio");
 var botaoInfo = document.getElementById("infoMenu");
 var botaoContato = document.getElementById("contato");
-var info = document.getElementById("contentText");
+var info = document.getElementById("text");
 var titulo = document.getElementById("titulo");
 
 var imgAtual = document.getElementById("foto");
@@ -40,24 +40,24 @@ function imgChange(direcao)
 
 
 //seta o conteudo de inicio
-info.textContent = `O projeto acamados + amados é uma iniciativa não
-  governamental que apoia familias e cuidadores de pessoas acamadas.`;
+  titulo.textContent = "Início"
+  info.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas.";
 
+//inicio
 botaoInicio.addEventListener("click", function(){
   console.log("inicio on");
   titulo.textContent = "Início"
-  info.textContent = `O projeto acamados + amados é uma iniciativa não
-    governamental que apoia familias e cuidadores de pessoas acamadas.`;
+  info.textContent = "O projeto acamados + amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas.";
   
 });
 
+//informações
 botaoInfo.addEventListener("click", function(){
-
   info.setAttribute('style', 'white-space: pre;');
 
   titulo.textContent = "Informações";
   info.textContent = "O projeto Acamados + Amados é uma iniciativa não governamental que apoia familias e cuidadores de pessoas acamadas. \r\n";
-  info.textContent += "Através de reuniões quinzenais o projeto vem distribuindo fraldas, alimentos equipamentos méidicos e também servindo como um grupo de apoio para os acamados e seus cuidadores. \r\n";
+  info.textContent += "Através de reuniões quinzenais o projeto vem distribuindo fraldas, alimentos equipamentos médicos e também servindo como um grupo de apoio para os acamados e seus cuidadores. \r\n";
   info.textContent += "Idealizado por Dona Inês, o projeto existe a mais de 15 anos e conta com  varios voluntarios e parceiros. \r\n";
   info.textContent += "Com a ajuda fornecida o projeto presta tambem atendimento odontologico e faz visitas regulares as casas cadastradas para";
   info.textContent += "se certificar que as orientações estão sendo seguidas e ajudar no que for necessario. \r\n";
@@ -66,6 +66,7 @@ botaoInfo.addEventListener("click", function(){
 
 });
 
+//contato
 botaoContato.addEventListener("click", function(){
 
   info.setAttribute('style', 'white-space: pre;');
